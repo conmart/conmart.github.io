4a4eda6b30dbd69eceb01f4c78a5487d94f825c2
conmart <connor.a.martinelli@gmail.com>
2024-07-01 17:14:49 -0700

    start breaking out javascript and fix variable error

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import { orbs } from './orbs.js';
+
 let boxWidth = 118,
   boxHeight = 118,
   boxTop = 8,
@@ -9,47 +11,6 @@ let boxWidth = 118,
   directions = ['se', 'sw', 'ne', 'nw'],
   speed = 2,
   timeout = null;
-
-const orbs = [
-  {
-    img: 'assets/nhII.png',
-    hoverBlurb: `<div>Technical Support and Software Engineering</div>
-      <div>2020-2023</div>`,
-    popupHtml: '<div>Test1</div>',
-  },
-  {
-    img: 'assets/rg.jpeg',
-    hoverBlurb: `<div>Technical Support and Software Engineering</div>
-      <div>2017-2020</div>`,
-    popupHtml: '<div>Test2</div>',
-  },
-  {
-    img: 'assets/NRDSII.png',
-    hoverBlurb: '<div>Sales and Partnerships</div><div>2014-2017</div>',
-    popupHtml: '<div>Test3</div>',
-  },
-  {
-    img: 'assets/Headshot1-small.jpg',
-    hoverBlurb: `
-      <div>Connor Martinelli</div>
-      <div>
-        <a href="mailto:connor.a.martinelli@gmail.com">email</a>
-        <a href="https://github.com/conmart" target="_blank">GitHub</a>
-      </div>`,
-    popupHtml: `
-      <div>Hello There! Thanks for visiting.</div>
-      <div>
-        <a href="mailto:connor.a.martinelli@gmail.com">email</a>
-        <a href="https://github.com/conmart" target="_blank">GitHub</a>
-      </div>`,
-  },
-  {
-    img: 'assets/script_cal2.jpg',
-    hoverBlurb: `<div>BA Psychology 2013</div>
-      <div>Relevant Courses: CS10, CS61A</div>`,
-    popupHtml: '<div>Test4</div>',
-  },
-];
 
 const createOrb = (orb) => {
   const div = document.createElement('DIV');
@@ -117,7 +78,7 @@ const stopOnHover = (div, orb) => {
 };
 
 const animateOrb = (div, orb) => {
-  request = requestAnimationFrame(() => animateOrb(div, orb));
+  const request = requestAnimationFrame(() => animateOrb(div, orb));
   orb['request'] = request;
   move(orb);
 };
@@ -129,13 +90,17 @@ const initialOrbPosition = (orb) => {
   orb['translateY'] = initialY;
 };
 
-let orbIndex = 0;
-orbs.forEach((orb) => {
-  const orbId = `orb${orbIndex}`;
-  orb['id'] = orbId;
-  orb['direction'] = directions[Math.floor(Math.random() * 4)];
-  createOrb(orb);
-  orbIndex++;
+
+
+window.addEventListener('load', () => {
+  let orbIndex = 0;
+  orbs.forEach((orb) => {
+    const orbId = `orb${orbIndex}`;
+    orb['id'] = orbId;
+    orb['direction'] = directions[Math.floor(Math.random() * 4)];
+    createOrb(orb);
+    orbIndex++;
+  });
 });
 
 window.addEventListener(
---
+++ orbs.js
@@ -0,0 +1,40 @@
+export const orbs = [
+  {
+    img: 'assets/nhII.png',
+    hoverBlurb: `<div>Technical Support and Software Engineering</div>
+      <div>2020-2023</div>`,
+    popupHtml: '<div>Test1</div>',
+  },
+  {
+    img: 'assets/rg.jpeg',
+    hoverBlurb: `<div>Technical Support and Software Engineering</div>
+      <div>2017-2020</div>`,
+    popupHtml: '<div>Test2</div>',
+  },
+  {
+    img: 'assets/NRDSII.png',
+    hoverBlurb: '<div>Sales and Partnerships</div><div>2014-2017</div>',
+    popupHtml: '<div>Test3</div>',
+  },
+  {
+    img: 'assets/Headshot1-small.jpg',
+    hoverBlurb: `
+      <div>Connor Martinelli</div>
+      <div>
+        <a href="mailto:connor.a.martinelli@gmail.com">email</a>
+        <a href="https://github.com/conmart" target="_blank">GitHub</a>
+      </div>`,
+    popupHtml: `
+      <div>Hello There! Thanks for visiting.</div>
+      <div>
+        <a href="mailto:connor.a.martinelli@gmail.com">email</a>
+        <a href="https://github.com/conmart" target="_blank">GitHub</a>
+      </div>`,
+  },
+  {
+    img: 'assets/script_cal2.jpg',
+    hoverBlurb: `<div>BA Psychology 2013</div>
+      <div>Relevant Courses: CS10, CS61A</div>`,
+    popupHtml: '<div>Test4</div>',
+  },
+];
