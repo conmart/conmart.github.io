5b7ce4f2a6b242fd2dc547b925dd10159c66a43a
conmart <connor.a.martinelli@gmail.com>
2024-07-01 17:59:50 -0700

    refactored code and fixed small positioning bug

---
+++ generateOrbs.js
@@ -0,0 +1,79 @@
+import { xMax, yMax, move } from './orbMovement.js';
+
+const initialOrbPosition = (orb) => {
+  const initialX = Math.floor(Math.random() * xMax + 1);
+  const initialY = Math.floor(Math.random() * yMax + 1);
+  orb['translateX'] = initialX;
+  orb['translateY'] = initialY;
+};
+
+const animateOrb = (div, orb) => {
+  const request = requestAnimationFrame(() => animateOrb(div, orb));
+  orb['request'] = request;
+  move(orb);
+};
+
+const stopOnHover = (div, orb) => {
+  let hoverClass = '';
+  div.addEventListener('mouseenter', () => {
+    hoverClass =
+      orb['translateX'] < xMax / 2 ? 'hoverBlurbRight' : 'hoverBlurbLeft';
+    window.cancelAnimationFrame(orb['request']);
+    div.firstElementChild.classList.add(hoverClass);
+  });
+  div.addEventListener('mouseleave', () => {
+    animateOrb(div, orb);
+    div.firstElementChild.classList.remove(hoverClass);
+  });
+};
+
+const showPopupEventListener = (div, popup, popupContainer) => {
+  div.addEventListener('click', () => {
+    popup.classList.add('showPopup');
+    popupContainer.classList.add('showPopup');
+  });
+};
+
+const popupToggle = (orb) => {
+  const popup = document.getElementById(orb['id'] + 'Popup');
+  const popupContainer = document.getElementById('popups');
+  const buttonTrigger = document.getElementById(orb['id'] + 'PopupTrigger');
+  const imageTrigger = document.getElementById(orb['id']).lastElementChild;
+  showPopupEventListener(buttonTrigger, popup, popupContainer);
+  showPopupEventListener(imageTrigger, popup, popupContainer);
+  document
+    .getElementById(orb['id'] + 'ClosePopup')
+    .addEventListener('click', () => {
+      popup.classList.remove('showPopup');
+      popupContainer.classList.remove('showPopup');
+    });
+};
+
+const createPopup = (orb) => {
+  const div = document.createElement('DIV');
+  div.setAttribute('class', 'popup');
+  div.setAttribute('id', orb['id'] + 'Popup');
+  div.innerHTML =
+    orb['popupHtml'] + `<button id="${orb['id']}ClosePopup">Close</button>`;
+  document.getElementById('popups').appendChild(div);
+  popupToggle(orb);
+};
+
+export const createOrb = (orb) => {
+  const div = document.createElement('DIV');
+  const buttonId = orb['id'] + 'PopupTrigger';
+  div.setAttribute('class', 'orbContainer');
+  div.setAttribute('id', orb['id']);
+  div.innerHTML = `
+    <div class="hoverBlurb">
+      ${orb['hoverBlurb']}
+      <div><button class="learnMore" id="${buttonId}">Learn More</button></div>
+    </div>
+    <div class="imgContainer"><img src="${orb['img']}" /></div>
+  `;
+  document.getElementById('mainContainer').appendChild(div);
+  createPopup(orb);
+  initialOrbPosition(orb);
+  animateOrb(div, orb);
+  stopOnHover(div, orb);
+};
---
+++ orbMovement.js
@@ -0,0 +1,120 @@
+let orbSidePx = 124,
+  xMin = 0,
+  yMin = 0,
+  speed = 2,
+  timeout = null;
+
+export let xMax = window.innerWidth - orbSidePx;
+export let yMax = window.innerHeight - orbSidePx;
+
+const setLimits = (orb) => {
+  let direction = orb['direction'];
+  if (orb['translateY'] <= yMin) {
+    if (direction == 'nw') {
+      direction = 'sw';
+    } else if (direction == 'ne') {
+      direction = 'se';
+    }
+  }
+  if (orb['translateY'] >= yMax) {
+    if (direction == 'se') {
+      direction = 'ne';
+    } else if (direction == 'sw') {
+      direction = 'nw';
+    }
+  }
+  if (orb['translateX'] <= xMin) {
+    if (direction == 'nw') {
+      direction = 'ne';
+    } else if (direction == 'sw') {
+      direction = 'se';
+    }
+  }
+  if (orb['translateX'] >= xMax) {
+    if (direction == 'ne') {
+      direction = 'nw';
+    } else if (direction == 'se') {
+      direction = 'sw';
+    }
+  }
+  orb['direction'] = direction;
+};
+
+const setDirection = (orb) => {
+  switch (orb['direction']) {
+    case 'ne':
+      orb['translateX'] += speed;
+      orb['translateY'] -= speed;
+      break;
+    case 'nw':
+      orb['translateX'] -= speed;
+      orb['translateY'] -= speed;
+      break;
+    case 'se':
+      orb['translateX'] += speed;
+      orb['translateY'] += speed;
+      break;
+    case 'sw':
+      orb['translateX'] -= speed;
+      orb['translateY'] += speed;
+      break;
+  }
+  setLimits(orb);
+};
+
+const getVendor = () => {
+  const ua = navigator.userAgent.toLowerCase(),
+    match =
+      /opera/.exec(ua) ||
+      /msie/.exec(ua) ||
+      /firefox/.exec(ua) ||
+      /(chrome|safari)/.exec(ua) ||
+      /trident/.exec(ua),
+    vendors = {
+      opera: '-o-',
+      chrome: '-webkit-',
+      safari: '-webkit-',
+      firefox: '-moz-',
+      trident: '-ms-',
+      msie: '-ms-',
+    };
+
+  return vendors[match[0]];
+};
+
+const setStyle = (orb, properties) => {
+  const element = document.getElementById(orb['id']);
+  const prefix = getVendor();
+  let property,
+    css = '';
+  for (property in properties) {
+    css += property + ': ' + properties[property] + ';';
+    css += prefix + property + ': ' + properties[property] + ';';
+  }
+  element.style.cssText += css;
+};
+
+const slider = document.getElementById('orbSpeed');
+slider.oninput = () => {
+  speed = parseInt(slider.value) / 2;
+};
+
+const updateWindowMinMax = () => {
+  xMax = window.innerWidth - orbSidePx;
+  yMax = window.innerHeight - orbSidePx;
+};
+
+window.addEventListener(
+  'resize',
+  () => {
+    clearTimeout(timeout);
+    timeout = setTimeout(updateWindowMinMax, 100);
+  },
+  false
+);
+
+export const move = (orb) => {
+  const transform = `translate3d(${orb['translateX']}px, ${orb['translateY']}px, 0)`;
+  setDirection(orb);
+  setStyle(orb, { transform });
+};
